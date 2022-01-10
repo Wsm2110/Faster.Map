fbe6836d1d2e2df85c47704b8fcb49419026ed3b
Wiljan Ruizendaal <wruizendaal@gmail.com>
2022-01-10 13:49:30 +0100

    Update Entry.cs

--- Core/Entry.cs
+++ Core/Entry.cs
@@ -1,13 +1,19 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace Faster.Core
+namespace Faster.Map.Core
 {
     [DebuggerDisplay("Key {Key} - value {Value}")]
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct Entry<TValue>
+    public struct Entry<TKey, TValue>
     {
-        public int Key { get; set; }
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        /// <value>
+        /// The key.
+        /// </value>
+        public TKey Key { get; set; }
 
         /// <summary>
         /// Gets or sets the value.
