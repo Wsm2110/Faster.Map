ed9ad718116dc04e3142febcd431dc2bf1a61bcd
Wiljan Ruizendaal <wruizendaal@gmail.com>
2023-01-07 17:07:25 +0100

    minor fix

--- src/DenseMapSIMD.cs
+++ src/DenseMapSIMD.cs
@@ -5,6 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics.X86;
 using System.Runtime.Intrinsics;
 using Faster.Map.Core;
+using System.Diagnostics;
 
 namespace Faster.Map
 {
